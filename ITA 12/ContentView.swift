ffc1d56dabb1f0851239c497a91218f628a99235
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-08-08 17:48:21 +0200

    Initial Commit

---
+++ ITA 12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ITA 12
+//
+//  Created by mcpeaps_HD on 08/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ITA 12/ITA_12App.swift
@@ -0,0 +1,17 @@
+//
+//  ITA_12App.swift
+//  ITA 12
+//
+//  Created by mcpeaps_HD on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ITA_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
